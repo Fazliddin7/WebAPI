054e85e1aa1cafd941625fd0e84661fe92503112
Fazliddin7 <=>
2022-06-15 03:39:27 +0600

    TestCommandBase impl IDisposable

--- Tests/Common/TestCommandBase.cs
+++ Tests/Common/TestCommandBase.cs
@@ -1,22 +1,31 @@
 ﻿using Application;
+using Infrastructure;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Tests.Common
 {
-    public class TestCommandBase
+    public class TestCommandBase: IDisposable
     {
         protected readonly FlightCache flightCache;
         protected readonly EfRepository efRepository;
+        private readonly ApplicationDbContext context;
         public TestCommandBase()
         {
-            efRepository = new EfRepository(FlightContextFactory.Create());
+            context = FlightContextFactory.Create();
+            efRepository = new EfRepository(context);
 
             var services = new ServiceCollection();
             services.AddMemoryCache();
             var serviceProvider = services.BuildServiceProvider();
             var memoryCache = serviceProvider.GetService<IMemoryCache>();
             flightCache = new FlightCache(efRepository, memoryCache);
+        }
+
+        public void Dispose()
+        {
+            FlightContextFactory.Destroy(context);
         }
     }
 }
